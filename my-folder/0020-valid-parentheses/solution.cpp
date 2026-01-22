18141c81ab889a3d2df6ea14ee7360b4cd2f4ed1
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-01-22 05:47:58 +0000

    [LeetCode Sync] Runtime - 3 ms (9.90%), Memory - 10 MB (8.17%)

---
+++ my-folder/0020-valid-parentheses/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (auto c : s) {
+            if (c == '(')
+                st.push(')');
+            else if (c == '{')
+                st.push('}');
+            else if (c == '[')
+                st.push(']');
+
+            else {
+                if (st.empty() || (!st.empty() && st.top() != c))
+                    return false;
+                else
+                    st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
