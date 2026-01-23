6400265e84cf8116afd33112eca6464e71a34cb4
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-01-23 04:33:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.8 MB (47.27%)

---
+++ my-folder/0242-valid-anagram/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>freq1(26,0),freq2(26,0);
+        if(s.size()!=t.size()) return false;
+        for(int i=0;i<s.size();i++)
+        {
+            freq1[s[i]-'a']++;
+            freq2[t[i]-'a']++;
+        }
+        return freq1==freq2;
+    }
+};
