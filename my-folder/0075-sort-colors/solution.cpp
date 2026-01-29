163d9159d4251c84dcf1da0bbb3142305f2ede0b
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-01-29 06:29:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.4 MB (97.30%)

---
+++ my-folder/0075-sort-colors/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int lo = 0;
+        int hi = n-1;
+        int mid = 0;
+        while(mid<=hi)
+        {
+            if(nums[mid]==0){
+                swap(nums[lo],nums[mid]);
+                lo++;
+                mid++;
+            }
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[hi]);
+                hi--;
+            }
+            else mid++;
+        }
+    }
+};
