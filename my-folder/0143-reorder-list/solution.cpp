7404978c3cd50dd46e4a9476e0c76982f547e1c6
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-01-31 07:31:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 22.7 MB (95.93%)

---
+++ my-folder/0143-reorder-list/solution.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    ListNode* reverse(ListNode* head)
+    {
+        ListNode* prev = NULL;
+        ListNode* curr= head;
+        while(curr)
+        {
+            ListNode* Next = curr->next;
+            curr->next= prev;
+            prev = curr;
+            curr = Next;
+        }
+        return prev;
+    }
+public:
+    void reorderList(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* l1 = head;
+        ListNode* l2 = reverse(slow->next);
+        slow->next= NULL;
+        while(l1 && l2)
+        {
+            ListNode* n1 = l1->next;
+            ListNode* n2 = l2->next;
+            l1->next = l2;
+            l2->next = n1;
+            l1 = n1;
+            l2 = n2;
+        }
+        
+
+    }
+};
