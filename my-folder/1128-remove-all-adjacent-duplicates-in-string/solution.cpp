760fdefa7d4894c6779c82e1f0a82a568a3208cb
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-02-11 17:25:28 +0000

    [LeetCode Sync] Runtime - 7 ms (51.25%), Memory - 14.2 MB (53.35%)

---
+++ my-folder/1128-remove-all-adjacent-duplicates-in-string/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>st;
+        for(auto c : s)
+        {
+           if(!st.empty() && st.top()==c){ while(!st.empty()&& st.top()==c)st.pop();}
+            else st.push(c);
+        }
+        string res = "";
+        while(!st.empty()) {res += st.top();st.pop();}
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
