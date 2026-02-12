add5b16aa646d5701a12dc42aacf3c9f2cc3d490
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-02-12 16:49:06 +0000

    [LeetCode Sync] Runtime - 17 ms (25.79%), Memory - 11.5 MB (41.62%)

--- my-folder/0076-minimum-window-substring/solution.cpp
+++ my-folder/0076-minimum-window-substring/solution.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        int n= s.size();
         unordered_map<char,int>need;
         for(auto c : t)
         {
             need[c]++;
         }
+        int required =need.size();
 
-        int required = need.size();
-       int have = 0;
-       unordered_map<char,int>window;
-       int l=0;
-       int start = 0;
-       int minLen = INT_MAX;
-       for(int r= 0 ;r<s.size();r++)
+        int l = 0;
+        int start = 0 ;
+        int minLen = INT_MAX;
+        int have =0;
+        unordered_map<char,int>window;
+        for(int r = 0;r<n;r++)
         {
-            char ch = s[r];
+            char ch  = s[r];
             window[ch]++;
-            if(need.count(ch) && window[ch]==need[ch]){
+            if(need[ch]==window[ch])
+            {
                 have++;
             }
-
             while(have==required)
             {
-                if(minLen> r-l+1)
+                if(minLen>r-l+1)
                 {
                     minLen = r-l+1;
-                    start = l;
+                    start =l;
                 }
                 char c = s[l];
                 window[c]--;
