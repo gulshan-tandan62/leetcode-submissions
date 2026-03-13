a704f11188832b9de50a39c2a7da172864ba2d2a
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-03-13 09:51:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (17.10%)

--- my-folder/0070-climbing-stairs/solution.cpp
+++ my-folder/0070-climbing-stairs/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
-    
+    int dynamic(int n,vector<int>& dp)
+    {
+        if(n<0) return 0;
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = dynamic(n-1,dp)+dynamic(n-2,dp);
+    }
 public:
     int climbStairs(int n) {
-       
-        vector<int>dp(n+1,0);
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2;i<=n;i++)
-        {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
+        if(n==1 || n==0) return n;
+        vector<int>dp(n+1,-1);
+        dynamic(n,dp);
         return dp[n];
     }
 };
