14e55f3c6757c904eb3a1b7074f4815e651ea9dc
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-03-18 06:53:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (5.02%)

--- my-folder/0733-flood-fill/solution.cpp
+++ my-folder/0733-flood-fill/solution.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int startColor = image[sr][sc];
+        if(color == startColor) return image;
+        image[sr][sc] = color;
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
         int n = image.size();
         int m = image[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<pair<int,int>>q;
-        int startColor = image[sr][sc];
-        image[sr][sc] = color;
         vis[sr][sc] = 1;
-        q.push({sr,sc});
         while(!q.empty())
         {
-            int r = q.front().first;
-            int c = q.front().second;
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
-            vector<pair<int,int>>directions = {{0,1},{1,0},{-1,0},{0,-1}};
+            vector<pair<int,int>>directions = {{1,0},{0,1},{-1,0},{0,-1}};
             for(int i=0;i<4;i++)
             {
-                int nr  = r+directions[i].first;
-                int nc = c+directions[i].second;
-                if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==startColor && !vis[nr][nc])
+                int nr = row+directions[i].first;
+                int nc = col + directions[i].second;
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && image[nr][nc]==startColor)
                 {
+                    image[nr][nc] = color;
                     vis[nr][nc] = 1;
                     q.push({nr,nc});
-                    image[nr][nc] = color;
                 }
             }
         }
