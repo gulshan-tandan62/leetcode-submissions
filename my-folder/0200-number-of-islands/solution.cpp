b9c948f733d14ae146c1d18ec827b8e531753617
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-03-18 06:43:11 +0000

    [LeetCode Sync] Runtime - 39 ms (11.70%), Memory - 28.9 MB (7.02%)

--- my-folder/0200-number-of-islands/solution.cpp
+++ my-folder/0200-number-of-islands/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
-   void island(int r, int c,vector<vector<int>>& vis,vector<vector<char>>& grid)
+    void bfs(int i,int j,int n,int m,vector<vector<int>>& vis,vector<vector<char>>& grid)
     {
         queue<pair<int,int>> q;
-        q.push({r,c});
-        vis[r][c] = 1;
-        vector<vector<int>>directions = {{1,0},{0,1},{-1,0},{0,-1}};
+        q.push({i,j});
+        vis[i][j] = 1;
         while(!q.empty())
         {
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
-            for(int i=0;i<4;i++)
+            vector<pair<int,int>>directions = {{0,1},{1,0},{-1,0},{0,-1}};
+            for(int l=0;l<4;l++)
             {
-                int nr  = row + directions[i][0];
-                int nc = col + directions[i][1];
-                if(nr>=0 && nc>=0 && nr<grid.size()&& nc<grid[0].size() && !vis[nr][nc] && grid[nr][nc]=='1')
+                int nr = row + directions[l].first;
+                int nc = col + directions[l].second;
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && grid[nr][nc]=='1')
                 {
                     vis[nr][nc] = 1;
                     q.push({nr,nc});
@@ -24,23 +24,19 @@ class Solution {
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int count= 0;
-        int n =grid.size();
+        int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
+        int count = 0;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m ;j++)
+            for(int j=0;j<m;j++)
             {
                 if(!vis[i][j] && grid[i][j]=='1')
-                {
-                    island(i,j,vis,grid);
-                    count++;
-                }
+               { count++;
+                bfs(i,j,n,m,vis,grid);}
             }
-
         }
         return count;
-
     }
 };
