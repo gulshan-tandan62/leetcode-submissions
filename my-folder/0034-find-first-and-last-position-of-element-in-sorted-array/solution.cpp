c8e2af388d7f1b5935ad82658562adee3ee3ada6
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-04-03 05:32:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.5 MB (53.67%)

--- my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -2,37 +2,34 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int> res = {-1, -1};
         int lo = 0;
-        int hi = n - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] == target) {
-                if (mid>0 && nums[mid - 1] == target)
-                    hi = mid - 1;
-                else {
-                    res[0] = mid;
-                    break;
-                }
+        int hi = n-1;
+        vector<int>res = {-1,-1};
+        while(lo<=hi)
+        {
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid]==target){
+                res[0] = mid;
+                hi = mid-1;
             }
-            else if (nums[mid]>target) hi = mid -  1;
-            else  lo = mid+1;
+            else if(nums[mid]<target) lo = mid+1;
+            else hi =mid-1;
         }
-         lo = 0;
-         hi = n - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] == target) {
-                if (mid<n-1 && nums[mid + 1] == target)
-                    lo = mid + 1;
-                else {
-                    res[1] = mid;
-                    break;
-                }
+        
+        lo = 0;
+        hi = n-1;
+        while(lo<=hi)
+        {
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid]==target){
+                res[1] = mid;
+                lo =mid+1;
             }
-            else if (nums[mid]>target) hi = mid -  1;
-            else  lo = mid+1;
+           else if(nums[mid]<target)lo = mid+1;
+            else hi = mid-1;
         }
+    
         return res;
+
     }
 };
