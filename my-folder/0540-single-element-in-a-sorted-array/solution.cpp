d1fde4b99353a287b6d6a1cc60c70642cac26ce8
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-04-09 05:56:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 26.3 MB (6.41%)

--- my-folder/0540-single-element-in-a-sorted-array/solution.cpp
+++ my-folder/0540-single-element-in-a-sorted-array/solution.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        int lo  = 0;
         int hi = n-1;
+        int lo = 0;
         while(lo<hi)
         {
             int mid = lo+(hi-lo)/2;
             if(mid%2!=0)
             {
-                mid--;
+                mid = mid-1;
+
             }
-            if(nums[mid]==nums[mid+1])lo=mid+2;
-            else hi = mid;
+            if(nums[mid]==nums[mid+1]){
+                lo = mid+2;
+            }
+            else {
+                hi = mid;
+            }
         }
         return nums[lo];
     }
